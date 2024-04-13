ce31f9de5dfb004eeb6f452a2eaf685757f679b5
amin1029384756 <aminalogaili@gmail.com>
2024-04-13 01:53:07 -0700

    fixed error in recognizing ascii values instead of strings

--- boot.asm
+++ boot.asm
@@ -13,22 +13,22 @@ print:
 end:
     mov ah, 0
     int 0x16
-    cmp al, 1
+    cmp al, "1"
     je help
     jg checkapps
     jl end
 welcomemessage:
     db "Welcome To Akiva! To Get Help, Press 1. To View Your Apps, Press 2. To Modify User Settings, Press 3. To Shut Down, Press 4.", 0
 checkapps:
-    cmp al, 2
+    cmp al, "2"
     je apps
     jg checksettings
 checksettings:
-    cmp al, 3
+    cmp al, "3"
     je settings
     jg checkshutdown
 checkshutdown:
-    cmp al, 4
+    cmp al, "4"
     je shutdown
     jg end
     jl end
