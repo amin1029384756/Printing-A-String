4c6084fc44ae967818fad2f5f56f48d366d69e71
amin1029384756 <aminalogaili@gmail.com>
2024-04-13 01:48:44 -0700

    init

--- boot.asm
+++ boot.asm
@@ -1,6 +1,6 @@
 [org 0x7c00]
 mov ah, 0x0e
-mov bx, helloworld
+mov bx, welcomemessage
 
 print:
     mov al, [bx]
@@ -11,9 +11,33 @@ print:
     jmp print
 
 end:
-
-helloworld:
-    db "Hello World!", 0
-
+    mov ah, 0
+    int 0x16
+    cmp al, 1
+    je help
+    jg checkapps
+    jl end
+welcomemessage:
+    db "Welcome To Akiva! To Get Help, Press 1. To View Your Apps, Press 2. To Modify User Settings, Press 3. To Shut Down, Press 4.", 0
+checkapps:
+    cmp al, 2
+    je apps
+    jg checksettings
+checksettings:
+    cmp al, 3
+    je settings
+    jg checkshutdown
+checkshutdown:
+    cmp al, 4
+    je shutdown
+    jg end
+    jl end
+help:
+    mov ah, 0x0e
+    mov al, "H"
+    int 0x10
+apps:
+settings:
+shutdown:
 times 510-($-$$) db 0
 dw 0xaa55
